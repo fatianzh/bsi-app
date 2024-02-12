a2c486e178c28a5ed51e6c8d4c31f414db4233de
fatianzh <fatianurzakiah11@gmail.com>
2024-02-12 22:53:23 +0700

    update FetchApi

--- src/screens/FetchApi.js
+++ src/screens/FetchApi.js
@@ -1,52 +1,55 @@
+// ProductList.js
+import React, { useEffect , useState} from 'react';
 import { View, Text, ActivityIndicator, FlatList } from 'react-native';
-import { connect } from 'react-redux';
-import React, { useEffect, useState} from 'react';
-import { fetchProducts } from '../action/actions'
+import { connect, useDispatch } from 'react-redux';
+import { fetchProducts } from '../actions/action';
 
-const IntegrateFetchAPI = ({products, loading, error, fetchProducts}) => {
+const IntegrateFetchAPI = ({ products, loading, error }) => {
+  const dispatch = useDispatch()
+  
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+ 
 
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+  const getMovies = async () => {
+    try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const getProducts = async () => {
-        try {
-            const response = await fetch ('https://dummyjson.com/products');
-            const json = await response.json()
-            setData(json.products);
-        } catch (error) {
-            console.error('err', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        getProducts();
-    }, []);
-
-    return (
-        <View style={{flex:1, padding: 24}}>
-            {isLoading ? (
-                <ActivityIndicator />
-            ) : (
-                <FlatList
-                    data={data}
-                    keyExtractor={({id}) => id}
-                    renderItem={({item}) => (
-                        <Text>
-                            {item.title}, {item.description}
-                        </Text>
-                    )}
-                />
-            )}
-        </View>
-    );
+  useEffect(() => {
+    fetchProducts()(dispatch);
+  }, []);
+  
+  return (
+    <View style={{flex: 1, padding: 24}}>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={products?.movies || []}
+          keyExtractor={({id}) => id}
+          renderItem={({item}) => (
+            <Text>
+              {item.title}, {item.releaseYear}
+            </Text>
+          )}
+        />
+      )}
+    </View>
+  );
 };
 
 const mapStateToProps = state => ({
-    products: state.products,
-    loading: state.loading,
-    error: state.error
+  products: state.products,
+  loading: state.loading,
+  error: state.error
 });
 
-export default connect(mapStateToProps, { fetchProducts })(IntegrateFetchAPI);
+export default connect(mapStateToProps)(IntegrateFetchAPI);
