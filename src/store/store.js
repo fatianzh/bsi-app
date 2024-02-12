04f35cb0de4614b105813cc55f0acaf1ef9f2a07
fatianzh <fatianurzakiah11@gmail.com>
2024-02-12 22:52:54 +0700

    add thunkMiddleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,12 @@
-import { createStore } from "redux";
-import rootReducer from "../reducers/reducer";
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import rootReducer from '../reducers/reducer';
+import * as thunkMiddleware from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware.thunk)
+//   composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
